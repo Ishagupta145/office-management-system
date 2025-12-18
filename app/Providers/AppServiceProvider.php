b06fdca81e67ccf8457e7debebaac6e646bc9e63
Isha Gupta <isha.gupta22@spit.ac.in>
2025-12-18 17:10:40 +0530

    Force HTTPS schema in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. Import the URL Facade
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Force HTTPS if the app is in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
