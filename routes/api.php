d60dcffd84c408415ff2d28c0cdebd2841855fe2
Isha Gupta <isha.gupta22@spit.ac.in>
2025-12-18 23:46:06 +0530

    api routes

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,2 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LocationController;
-use App\Http\Controllers\EmployeeController;
-
-// Location APIs
-Route::prefix('locations')->group(function () {
-    Route::get('/countries', [LocationController::class, 'getCountries']);
-    Route::get('/states/{country}', [LocationController::class, 'getStates']);
-    Route::get('/cities/{state}', [LocationController::class, 'getCities']);
-});
-
-// Manager by company API
-Route::get('/companies/{companyId}/managers', [EmployeeController::class, 'getManagersByCompany']);
-
+// Intentionally left empty for Vercel deployment
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\EmployeeController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
+
+/*
+| Web Routes (Pages)
+*/
 
 Route::get('/', function () {
     return redirect()->route('companies.index');
 });
 
-// Company Routes
 Route::resource('companies', CompanyController::class);
-
-// Employee Routes
 Route::resource('employees', EmployeeController::class);
 
+/*
+| API Routes
+*/
+
+Route::prefix('locations')->group(function () {
+    Route::get('/countries', [LocationController::class, 'getCountries']);
+    Route::get('/states/{country}', [LocationController::class, 'getStates']);
+    Route::get('/cities/{state}', [LocationController::class, 'getCities']);
+});
+
+Route::get('/companies/{companyId}/managers', [EmployeeController::class, 'getManagersByCompany']);
